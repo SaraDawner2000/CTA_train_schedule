70cbb28e15eeb44bf6a511a0e1e1403278764727
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-03-07 21:11:31 +0000

    create landing page

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
+require "sinatra/activerecord"
+require "csv"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  stops_array = CSV.read("stops.csv")
+  parent_stations_array = stops_array.select {|stop| stop[0].match(/^4\d{4}$/)}
+  sorted_parent_stations_array = parent_stations_array.sort {|a, b| a[2] <=> b[2]}
+  @parent_stations_hash = sorted_parent_stations_array.to_h {|stop| [stop[0], stop[2]]}
+  erb(:landing_page)
+end
+get("/result") do
+  erb(:result)
 end
