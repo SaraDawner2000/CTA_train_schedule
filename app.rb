f908b43f931a3797d925f3574ec918a464f7851a
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-03-08 20:04:18 +0000

    Complete functional part of app

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require "sinatra"
 require "sinatra/reloader"
 require "sinatra/activerecord"
+require "sinatra/cookies"
 require "csv"
 require "http"
+require "date"
 
 get("/") do
   stops_array = CSV.read("stops.csv")
@@ -12,15 +14,42 @@ get("/") do
   erb(:landing_page)
 end
 get("/result") do
+  @line_direction_hash = {
+    "Red" => {"1" => "Howard-bound", "5" => "95th/Dan Ryan-bound"},
+    "P" => {"1" => "Linden-bound", "5" => "Howard- or Loop-bound"},
+    "Y" => {"1" => "Skokie-bound", "5" => "Howard-bound"},
+    "Blue" => {"1" => "O\'Hare-bound", "5" => "Forest Park-bound"},
+    "Pink" => {"1" => "Loop-bound", "5" => "54th/Cermak-bound"},
+    "G" => {"1" => "Harlem/Lake-bound", "5" => "Ashland/63rd- or Cottage Grove-bound"},
+    "Org" => {"1" => "Loop-bound", "5" => "Midway-bound"},
+    "Brn" => {"1" => "Kimball-bound", "5" => "Loop-bound"},
+  }
+  @line_name_hash = {
+    "Red" => "Red Line",
+    "P" => "Purple Line",
+    "Y" => "Yellow Line",
+    "Blue" => "Blue Line",
+    "Pink" => "Pink Line",
+    "G" => "Green Line",
+    "Org" => "Orange Line",
+    "Brn" => "Brown Line",
+  }
   direction = params["direction"]
-  station_id = params["station"]
+  @station_id = params["station"]
   @station = $parent_stations_hash
-  @station_name = @station[station_id]
-  api_url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=#{ENV["CTA_API_KEY"]}&mapid=#{station_id}&outputType=JSON"
+  @station_name = @station[@station_id]
+  cookies.store("station", @station_id)
+  api_url = "http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?key=#{ENV["CTA_API_KEY"]}&mapid=#{@station_id}&outputType=JSON"
   raw_data = HTTP.get(api_url)
   raw_data_string = raw_data.to_s
   @parsed_data = JSON.parse(raw_data_string)
-  eta_times = @parsed_data["ctatt"]["eta"]
-  line_color = @parsed_data
+  @eta_times = @parsed_data["ctatt"]["eta"]
+  @lines = @eta_times.collect{|instance| instance["rt"]}.uniq
+
+  @lines_hashed = @lines.map{|line| [line, 
+  {"1" => @eta_times.select {|instance| instance["rt"] == line && instance["trDr"] == "1"},
+  "5" => @eta_times.select {|instance| instance["rt"] == line && instance["trDr"] == "5"}}
+  ]}.to_h
+
   erb(:result)
 end
